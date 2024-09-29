138bc49f8d46dc9f46fd3625f2dbf063ab116477
Kuma Bayne <kuma88@me.com>
2024-09-29 14:17:45 -0700

    Update resume

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -7,86 +7,86 @@ import Container from "./Container";
 import Logo from "./Logo";
 
 function Navigation() {
-    const navRef = useRef(null);
-    const links = [
-        { id: 1, href: "#skills", text: "about" },
-        { id: 2, href: "#work", text: "work" },
-        {
-            id: 3,
-            download: true,
-            href: "/files/kuma-bayne_resume.pdf",
-            text: "resume",
-            target: "_blank",
-        },
-        {
-            id: 4,
-            icon: BsLinkedin,
-            href: "https://www.linkedin.com/in/kuma-bayne-1b83b7168/",
-            target: "_blank",
-        },
-        {
-            id: 5,
-            icon: BsGithub,
-            href: "https://github.com/kumabayne",
-            target: "_blank",
-        },
-    ];
+  const navRef = useRef(null);
+  const links = [
+    { id: 1, href: "#skills", text: "about" },
+    { id: 2, href: "#work", text: "work" },
+    {
+      id: 3,
+      download: true,
+      href: "/files/Kuma-Bayne_Resume.pdf",
+      text: "resume",
+      target: "_blank",
+    },
+    {
+      id: 4,
+      icon: BsLinkedin,
+      href: "https://www.linkedin.com/in/kuma-bayne-1b83b7168/",
+      target: "_blank",
+    },
+    {
+      id: 5,
+      icon: BsGithub,
+      href: "https://github.com/kumabayne",
+      target: "_blank",
+    },
+  ];
 
-    const changeNav = () => {
-        if (window.scrollY >= 8) {
-            navRef.current.classList.add("bg-black/40");
-        } else {
-            navRef.current.classList.remove("bg-black/40");
-        }
-    };
+  const changeNav = () => {
+    if (window.scrollY >= 8) {
+      navRef.current.classList.add("bg-black/40");
+    } else {
+      navRef.current.classList.remove("bg-black/40");
+    }
+  };
 
-    useEffect(() => {
-        changeNav();
-        window.addEventListener("scroll", changeNav);
+  useEffect(() => {
+    changeNav();
+    window.addEventListener("scroll", changeNav);
 
-        return () => window.removeEventListener("scroll", changeNav);
-    }, []);
+    return () => window.removeEventListener("scroll", changeNav);
+  }, []);
 
-    return (
-        <nav
-            ref={navRef}
-            className="backdrop-blur-md duration-500 ease-in-out py-4 transition-colors"
-        >
-            <Container>
-                <div className="flex items-center justify-between">
-                    <Link href="/">
-                        <Logo className="fill-indigo-200 hover:-scale-x-100" />
-                    </Link>
-                    <ul className="flex gap-6 items-center">
-                        {links.map((link) => (
-                            <li key={link.id}>
-                                <Link
-                                    className="cursor-pointer duration-300 ease-in-out font-medium text-indigo-100 text-sm transition-colors hover:text-indigo-300"
-                                    href={link.href}
-                                    target={link.target ? "_blank" : "_self"}
-                                    {...(link.download && { download: true })}
-                                >
-                                    {link.text ? (
-                                        <>
-                                            {link.text}
-                                            <span className="font-semibold pl-0.5 text-blue-400">
-                                                .
-                                            </span>
-                                        </>
-                                    ) : (
-                                        <link.icon
-                                            className="text-white hover:text-indigo-300"
-                                            size="18"
-                                        />
-                                    )}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </Container>
-        </nav>
-    );
+  return (
+    <nav
+      ref={navRef}
+      className="backdrop-blur-md duration-500 ease-in-out py-4 transition-colors"
+    >
+      <Container>
+        <div className="flex items-center justify-between">
+          <Link href="/">
+            <Logo className="fill-indigo-200 hover:-scale-x-100" />
+          </Link>
+          <ul className="flex gap-6 items-center">
+            {links.map((link) => (
+              <li key={link.id}>
+                <Link
+                  className="cursor-pointer duration-300 ease-in-out font-medium text-indigo-100 text-sm transition-colors hover:text-indigo-300"
+                  href={link.href}
+                  target={link.target ? "_blank" : "_self"}
+                  {...(link.download && { download: true })}
+                >
+                  {link.text ? (
+                    <>
+                      {link.text}
+                      <span className="font-semibold pl-0.5 text-blue-400">
+                        .
+                      </span>
+                    </>
+                  ) : (
+                    <link.icon
+                      className="text-white hover:text-indigo-300"
+                      size="18"
+                    />
+                  )}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Container>
+    </nav>
+  );
 }
 
 export default Navigation;
